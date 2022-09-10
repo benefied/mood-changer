d5a9a0dc2d8ab6a8de03562e689c9226af0159fc
Ojekudo Benedict <59806784+benefied@users.noreply.github.com>
2022-09-11 00:29:51 +0100

    comments added

--- moodChanger.sol
+++ moodChanger.sol
@@ -4,9 +4,12 @@ pragma solidity^ 0.8.7;
 
 contract createMood {
     string[] public moodList;
+    //function to create mood and add to mood array
     function addMood(string memory _mood) public{
         moodList.push(_mood);
     }
+    
+    //function to return the moodlist array
     function returnMood() public view returns(string[] memory){
         return moodList;
     }
